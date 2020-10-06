c1a85e9b67de7f50240922f1aa5a404644110d7a
Asli Y <aslihancap@gmail.com>
2020-10-06 20:59:13 +0100

    added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,7 +3,12 @@ public class AwesomeTest {
     // red means it s untacked
     // green means it s under staging area
     // grey in .idea folder means it's ignore
+    // when the color is normal, it means it s commited no changes
+
+    // i just pulled the new change from Github that i dod not have locally
+    // and now my project is in sync with remote repository
 
 
+    // now i made more awesome changes and I am about to push this back to Github
 
 }
